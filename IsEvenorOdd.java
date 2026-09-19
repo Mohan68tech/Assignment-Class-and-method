73031e25ecf53a30810f8869caf9b23afafed9ba
Mohan68tech <mohanjaykar688@gmail.com>
2026-09-19 19:12:51 +0530

    Add files via upload

---
+++ IsEvenorOdd.java
@@ -0,0 +1,12 @@
+ class IsEvenorOdd {
+   public static boolean isEven(int a){
+        if(a % 2 ==0){
+   return true;
+        }
+        return false;
+    }
+    public static void main(String[] args){
+      boolean result = isEven(5);
+      System.out.println(result);
+    }
+ }
